daad425ecc93dcb9a08fbaeb416ef9347dde73c5
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2025-06-12 21:40:49 +0300

    main: refactor code: make one fetch function

---
+++ helpers/FetchOzonSellerApi.php
@@ -0,0 +1,57 @@
+<?php
+
+$HOME = strlen($_SERVER['DOCUMENT_ROOT']) != 0 ? $_SERVER['DOCUMENT_ROOT'] : $_SERVER['PHP_CRON_HOME'];
+
+include_once "$HOME/env.php";
+
+class FetchOzonSellerApi {
+    static function fetchJson_byUriAndData($uri, $data) {
+        global $env;
+
+        $FETCH_URL = "https://api-seller.ozon.ru$uri";
+
+        $json_string = json_encode($data);
+        $http_data = $json_string;
+
+        $ozonClientId = $env['ozon-client-id'];
+        $ozonApiKey = $env['ozon-api-key'];
+
+        $http_headers = [
+            "Content-Type: application/json",
+            "Client-Id: $ozonClientId",
+            "Api-Key: $ozonApiKey",
+        ];
+
+        $http_cookie = implode("; ", [
+            "Client-Id=$ozonClientId",
+            "Api-Key=$ozonApiKey",
+        ]);
+
+        if ($env['log']) {
+            echo "\nPOST $FETCH_URL\n$http_data\n";
+        }
+
+        $ch = curl_init($FETCH_URL);                            // Инициализируем cURL сессии
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);                   // Устанавливаем метод POST
+        if ($data != null) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $http_data);       // Тело запроса
+        }
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $http_headers);    // Устанавливаем заголовки
+        curl_setopt($ch, CURLOPT_COOKIE, $http_cookie);         // Передаем куки
+        $response = curl_exec($ch);                             // Выполняем запрос и получаем ответ
+
+        if (curl_errno($ch)) {                                  // Проверяем на наличие ошибок
+            $err = curl_error($ch);                             // Получаем сообщение об ошибке
+            curl_close($ch);                                    // Закрываем cURL сессию
+            throw new Error("Fetch error: $err");
+        }
+
+        curl_close($ch);                                        // Закрываем cURL сессию
+        $json_string = $response;
+        $php_object = json_decode($json_string, true);
+        return $php_object;
+    }
+}
--- helpers/save-api-to-json/SaveJson_v1_returns_list.php
+++ helpers/save-api-to-json/SaveJson_v1_returns_list.php
@@ -2,7 +2,7 @@
 
 $HOME = strlen($_SERVER['DOCUMENT_ROOT']) != 0 ? $_SERVER['DOCUMENT_ROOT'] : $_SERVER['PHP_CRON_HOME'];
 
-include_once "$HOME/env.php";
+include_once "$HOME/helpers/FetchOzonSellerApi.php";
 
 class SaveJson_v1_returns_list {
     static function main() {
@@ -49,16 +49,16 @@ class SaveJson_v1_returns_list {
         $end = new DateTime($endDate);
         $end->modify('last day of this month'); // Получаем последний день месяца для конечной даты
         $data = [];
-    
+
         while ($start <= $end) {
             $startOfMonth = $start->format('Y-m-01\T00:00:00.000\Z');
             $endOfMonth = $start->format('Y-m-t\T23:59:59.999\Z');
-    
+
             $data[] = [
                 'start' => $startOfMonth,
                 'end' => $endOfMonth
             ];
-    
+
             // Переход к следующему месяцу
             $start->modify('first day of next month');
         }
@@ -80,47 +80,9 @@ class SaveJson_v1_returns_list {
     }
 
     static function fetchJson($data) {
-        global $env;
-
-        $URI = "/v1/returns/list";
-        $FETCH_URL = "https://api-seller.ozon.ru$URI";
-
-        $json_string = json_encode($data);
-        $http_data = $json_string;
-
-        $ozonClientId = $env['ozon-client-id'];
-        $ozonApiKey = $env['ozon-api-key'];
-
-        $http_headers = [
-            "Content-Type: application/json",
-            "Client-Id: $ozonClientId",
-            "Api-Key: $ozonApiKey",
-        ];
-
-        $http_cookie = implode("; ", [
-            "Client-Id=$ozonClientId",
-            "Api-Key=$ozonApiKey",
-        ]);
-
-        $ch = curl_init($FETCH_URL);                            // Инициализируем cURL сессии
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, true);                   // Устанавливаем метод POST
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $http_data);       // Тело запроса
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $http_headers);    // Устанавливаем заголовки
-        curl_setopt($ch, CURLOPT_COOKIE, $http_cookie);         // Передаем куки
-        $response = curl_exec($ch);                             // Выполняем запрос и получаем ответ
-
-        if (curl_errno($ch)) {                                  // Проверяем на наличие ошибок
-            $err = curl_error($ch);                             // Получаем сообщение об ошибке
-            curl_close($ch);                                    // Закрываем cURL сессию
-            throw new Error("Fetch error: $err");
-        }
-
-        curl_close($ch);                                        // Закрываем cURL сессию
-        $json_string = $response;
-        $php_object = json_decode($json_string, true);
-        return $php_object;
+        return FetchOzonSellerApi::fetchJson_byUriAndData(
+            "/v1/returns/list",
+            $data,
+        );
     }
 }
